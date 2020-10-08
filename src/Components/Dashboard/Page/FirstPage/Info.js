62df0c76b482022b8cb7429fa852067018c40513
Joshua Oyeleke <oyelekeoluwasayo@gmail.com>
2020-10-08 22:53:55 +0100

    Updated the local database

--- src/Components/Dashboard/Page/FirstPage/Info.js
+++ src/Components/Dashboard/Page/FirstPage/Info.js
@@ -2,17 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-// import database from '../../db';
-
-// const DATA = database;
-// console.log(DATA);
-
 const Info = () => {
 	const [items, setItems] = useState([]);
-	// const [currentData, setCurrentData] = useState([]);
 	const [count, setCount] = useState(0);
 	const [total, setTotal] = useState(0);
-
 
 	function handleRightClick() {
 		if (count === total - 1) {
@@ -31,14 +24,11 @@ const Info = () => {
 	}
 
 	useEffect(() => {
-		// if (items)
-		// console.log(items);
 		axios
 			.get("../../db.json")
 			.then((res) => {
-				console.log(res);
 				setItems(res.data.firstPage[count]);
-				setTotal(res.data.firstPage.length)
+				setTotal(res.data.firstPage.length);
 			})
 			.catch((err) => console.log(err));
 
@@ -47,129 +37,119 @@ const Info = () => {
 		};
 	}, [count]);
 
-	console.log(items);
-	console.log(items[count]);
-	return (
-		<div className="border-bg p-1 p-sm-2 info">
-			<h3 className="font-neogrey font-weight-bold font-italic text-center my-4 mb-lg-5">Global Network</h3>
-			<div className="p__abs d-flex justify-content-between align-items-center">
-				<div className="d-flex flex-nowrap ab">
-					<span className="bg-pry text-logo spn d-inline-block rounded-circle" onClick={handleLeftClick}>
-						<i className="fa fa-arrow-left"></i>
-					</span>
-					<span className="bg-logo rounded spn d-inline-block bg">{items.navPage1}</span>
-				</div>
+	if (items.length !== 0) {
+		return (
+			<div className="border-bg p-1 p-sm-2 info">
+				<h3 className="font-neogrey font-weight-bold font-italic text-center my-4 mb-lg-5">Global Network</h3>
+				<div className="p__abs d-flex justify-content-between align-items-center">
+					<div className="d-flex flex-nowrap ab">
+						<span className="bg-pry text-logo spn d-inline-block rounded-circle" onClick={handleLeftClick}>
+							<i className="fa fa-arrow-left"></i>
+						</span>
+						<span className="bg-logo rounded spn d-inline-block bg">{items.navPage1}</span>
+					</div>
 
-				<div className="d-flex flex-column justify-content-center align-items-center">
-					<button className="btn bg-transparent border py-1 py-sm-2 text-logo bd text-right mr-md-5 mr-xl-0" type="btn">
-						<i className="fa fa-arrow-up"></i>&nbsp;ID
-					</button>
-					<div className="d-flex flex-column flex-md-row py-2 justify-content-center align-items-center">
-						<div className="col d-flex justify-content-center align-items-center flex-md-wrap">
-							<div className="d-flex flex-column justify-content-between align-items-center">
-								<Link className="text-white" to="/home/page1">
-									<i className="fa fa-caret-up"></i>
-								</Link>
-								<span className="text-pry text-uppercase">History</span>
-								<Link className="text-white" to="/home/page1">
-									<i className="fa fa-caret-down"></i>
-								</Link>
+					<div className="d-flex flex-column justify-content-center align-items-center">
+						<button className="btn bg-transparent border py-1 py-sm-2 text-logo bd text-right mr-md-0" type="btn">
+							<i className="fa fa-arrow-up"></i>&nbsp;ID
+						</button>
+						<div className="d-flex flex-column flex-md-row py-2 justify-content-center align-items-center">
+							<div className="col d-flex justify-content-center align-items-center flex-md-wrap">
+								<div className="d-flex flex-column justify-content-between align-items-center">
+									<Link className="text-white" to="/home/page1">
+										<i className="fa fa-caret-up"></i>
+									</Link>
+									<span className="text-pry text-uppercase">History</span>
+									<Link className="text-white" to="/home/page1">
+										<i className="fa fa-caret-down"></i>
+									</Link>
+								</div>
+								<select className="mx-1 rounded" name="history" id="history">
+									<option value="1">1</option>
+									<option value="2">2</option>
+								</select>
+								<i className="fa fa-database text-pry"></i>
+								<i className="fa fa-refresh text-pry px-1 px-sm-2"></i>
+								<span>{items.history}</span>
 							</div>
-							<select className="mx-1 rounded" name="history" id="history">
-								<option value="1">1</option>
-								<option value="2">2</option>
-							</select>
-							<i className="fa fa-database text-pry"></i>
-							<i className="fa fa-refresh text-pry px-1 px-sm-2"></i>
-							<span>{items.history}</span>
-						</div>
 
-						<div className="">
-							<button className="btn rounded bg-logo px-1 px-sm-2 id__details d-flex flex-column justify-content-center align-items-between">
-								<span className="w-100 d-flex justify-content-between">
-									<span>{items.id}</span>
-									<span className="text-white">ID 7860</span>
-								</span>
-								<span className="w-100 text-white d-flex justify-content-end py-1">
-									<span className="bg-pry p-1 shadow-lg rounded bg-dark-logo">
-										$ <span>{items.idAmt}</span>
+							<div className="">
+								<button className="btn rounded bg-logo px-1 px-sm-2 id__details d-flex flex-column justify-content-center align-items-between">
+									<span className="w-100 d-flex justify-content-between">
+										<span>{items.id}</span>
+										<span className="text-white">ID 7860</span>
 									</span>
-									<span className="bg-pry p-1 shadow-lg rounded bg-dark-logo ml-1">
-										{items.stellarAmt} <span>XLM</span>
+									<span className="w-100 text-white d-flex justify-content-end py-1">
+										<span className="bg-pry p-1 shadow-lg rounded bg-dark-logo">
+											$ <span>{items.idAmt}</span>
+										</span>
+										<span className="bg-pry p-1 shadow-lg rounded bg-dark-logo ml-1">
+											{items.stellarAmt} <span>XLM</span>
+										</span>
 									</span>
-								</span>
-							</button>
-							<div className="border mt-3 gbn__h d-flex flex-column rounded">
-								<div className="w-100 p-2 bg-light-col rounded d-flex justify-content-between align-items-end text-center">
-									<div className="dt rounded-circle">
-										<Link className="d-inline-block w-100 h-100 bg-pry rounded-circle" to="/home/page1"></Link>
-									</div>
-									<div className="d-flex justify-content-end">
-										<div>
-											<i className="fa fa-users text-logo"></i>
-											<span className="pl-1">0</span>
+								</button>
+								<div className="border mt-3 gbn__h d-flex flex-column rounded">
+									{items.people.map((item) => (
+										<div
+											key={item.id}
+											className={`w-100 p-2 bg-light-col rounded d-flex justify-content-between align-items-end text-center ${
+												item.id !== 1 ? "border-top" : ""
+											}`}
+										>
+											<div className="dt rounded-circle">
+												{item.link ? (
+													<Link
+														className="d-inline-block w-100 h-100 bg-pry rounded-circle"
+														to="/home/page1"
+													></Link>
+												) : (
+													""
+												)}
+											</div>
+											<div className="d-flex justify-content-end">
+												<div>
+													<i className="fa fa-users text-logo"></i>
+													<span className="pl-1">{item.users}</span>
+												</div>
+												<div className="pl-1">
+													<i className="fa fa-gift text-pry"></i>
+													<span className="pl-1">{item.gift}</span>
+												</div>
+											</div>
 										</div>
-										<div className="pl-1">
-											<i className="fa fa-gift text-pry"></i>
-											<span className="pl-1">0</span>
-										</div>
-									</div>
-								</div>
-								<div className="w-100 p-2 bg-light-col rounded border-top d-flex justify-content-between align-items-end text-center">
-									<div className="dt rounded-circle"></div>
-									<div className="d-flex justify-content-end">
-										<div>
-											<i className="fa fa-users text-logo"></i>
-											<span className="pl-1">0</span>
-										</div>
-										<div className="pl-1">
-											<i className="fa fa-gift text-pry"></i>
-											<span className="pl-1">0</span>
-										</div>
-									</div>
-								</div>
-								<div className="w-100 p-2 bg-light-col rounded border-top d-flex justify-content-between align-items-end text-center">
-									<div className="dt rounded-circle"></div>
-									<div className="d-flex justify-content-end">
-										<div>
-											<i className="fa fa-users text-logo"></i>
-											<span className="pl-1">0</span>
-										</div>
-										<div className="pl-1">
-											<i className="fa fa-gift text-pry"></i>
-											<span className="pl-1">0</span>
-										</div>
-									</div>
+									))}
 								</div>
 							</div>
-						</div>
 
-						<div className="col d-flex justify-content-center align-items-center">
-							<div className="text-center">
-								<i className="fa fa-users text-logo"></i>
-								<span className="pl-1">{items.item1}</span>
-							</div>
-							<div className="pl-1 text-center">
-								<i className="fa fa-gift text-pry"></i>
-								<span className="pl-1">{items.item2}</span>
-							</div>
-							<div className="pl-1 text-center">
-								<i className="fa fa-minus bg-pry rounded-circle"></i>
-								<span className="pl-1">{items.item3}</span>
+							<div className="col d-flex justify-content-center align-items-center">
+								<div className="text-center">
+									<i className="fa fa-users text-logo"></i>
+									<span className="pl-1">{items.item1}</span>
+								</div>
+								<div className="pl-1 text-center">
+									<i className="fa fa-gift text-pry"></i>
+									<span className="pl-1">{items.item2}</span>
+								</div>
+								<div className="pl-1 text-center">
+									<i className="fa fa-minus bg-pry rounded-circle"></i>
+									<span className="pl-1">{items.item3}</span>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
 
-				<div className="d-flex flex-nowrap ab">
-					<span className="bg-logo rounded spn d-inline-block bg">{items.navPage2}</span>
-					<span className="bg-pry text-logo spn d-inline-block rounded-circle" onClick={handleRightClick}>
-						<i className="fa fa-arrow-right"></i>
-					</span>
+					<div className="d-flex flex-nowrap ab">
+						<span className="bg-logo rounded spn d-inline-block bg">{items.navPage2}</span>
+						<span className="bg-pry text-logo spn d-inline-block rounded-circle" onClick={handleRightClick}>
+							<i className="fa fa-arrow-right"></i>
+						</span>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	} else {
+		return <div>Loading...</div>;
+	}
 };
 
 export default Info;
--- src/Components/Dashboard/Page/SecondPage/Info.js
+++ src/Components/Dashboard/Page/SecondPage/Info.js
@@ -1,120 +1,206 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Info = () => {
-	return (
-		<div className="border-bg p-1 p-sm-2 info second__box">
-			<h3 className="font-neogrey font-weight-bold font-italic text-center my-4 mb-lg-5">Global Network</h3>
-			<div className="p__abs d-flex justify-content-between align-items-center">
-				<div className="d-flex flex-nowrap ab">
-					<Link to="/home/page2" className="bg-pry text-logo spn d-inline-block rounded-circle">
-						<i className="fa fa-arrow-left"></i>
-					</Link>
-					<span className="bg-logo rounded spn d-inline-block bg">15</span>
-				</div>
+	const [items, setItems] = useState([]);
+	const [count, setCount] = useState(0);
+	const [total, setTotal] = useState(0);
 
-				<div className="d-flex flex-column flex-md-row py-2 justify-content-center align-items-center w-100 max__w">
-					<div className="col-md-4 d-flex justify-content-center align-items-center flex-md-wrap">
-						<div className="d-flex flex-column justify-content-between align-items-center">
-							<Link className="text-white" to="/home/page2">
-								<i className="fa fa-caret-up"></i>
-							</Link>
-							<span className="text-pry text-uppercase">History</span>
-							<Link className="text-white" to="/home/page2">
-								<i className="fa fa-caret-down"></i>
-							</Link>
-						</div>
-						<select className="mx-1 rounded" name="history" id="history">
-							<option value="1">1</option>
-							<option value="2">2</option>
-						</select>
-						<i className="fa fa-database text-pry"></i>
-						<i className="fa fa-refresh text-pry px-1 px-sm-2"></i>
-						<span>444</span>
+	function handleRightClick() {
+		if (count === total - 1) {
+			setCount(0);
+		} else {
+			setCount(count + 1);
+		}
+	}
+
+	function handleLeftClick() {
+		if (count === 0) {
+			setCount(total - 1);
+		} else {
+			setCount(count - 1);
+		}
+	}
+
+	useEffect(() => {
+		axios
+			.get("../../db.json")
+			.then((res) => {
+				setItems(res.data.secondPage[count]);
+				setTotal(res.data.secondPage.length);
+			})
+			.catch((err) => console.log(err));
+
+		return () => {
+			console.log("unmounting");
+		};
+	}, [count]);
+
+	if (items.length !== 0) {
+		console.log(items.people[0].id === 1);
+		return (
+			<div className="border-bg p-1 p-sm-2 info second__box">
+				<h3 className="font-neogrey font-weight-bold font-italic text-center my-4 mb-lg-5">Global Network</h3>
+				<div className="p__abs d-flex justify-content-between align-items-center">
+					<div className="d-flex flex-nowrap ab">
+						<span className="bg-pry text-logo spn d-inline-block rounded-circle" onClick={handleLeftClick}>
+							<i className="fa fa-arrow-left"></i>
+						</span>
+						<span className="bg-logo rounded spn d-inline-block bg">{items.id}</span>
 					</div>
 
-					<div className="col-md-4">
-						<button className="btn w-100 rounded mb-2 bg-transparent border py-1 py-sm-2 text-logo text-right" type="btn">
-							<i className="fa fa-arrow-up"></i>&nbsp;ID
-						</button>
+					<div className="d-flex flex-column flex-md-row py-2 justify-content-center align-items-center w-100 max__w">
+						<div className="col-md-4 d-flex justify-content-center align-items-center flex-md-wrap">
+							<div className="d-flex flex-column justify-content-between align-items-center">
+								<Link className="text-white" to="/home/page2">
+									<i className="fa fa-caret-up"></i>
+								</Link>
+								<span className="text-pry text-uppercase">History</span>
+								<Link className="text-white" to="/home/page2">
+									<i className="fa fa-caret-down"></i>
+								</Link>
+							</div>
+							<select className="mx-1 rounded" name="history" id="history">
+								<option value="1">1</option>
+								<option value="2">2</option>
+							</select>
+							<i className="fa fa-database text-pry"></i>
+							<i className="fa fa-refresh text-pry px-1 px-sm-2"></i>
+							<span>{items.history}</span>
+						</div>
 
-						<button className="btn w-100 rounded bg-logo px-1 px-sm-2 d-flex flex-column justify-content-center align-items-between">
-							<span className="w-100 d-flex justify-content-between">
-								<span>1</span>
-								<span className="text-white">ID 7860</span>
-							</span>
-							<span className="w-100 text-white d-flex justify-content-end py-1">
-								<span className="bg-pry p-1 shadow-lg rounded bg-dark-logo">
-									$ <span>0</span>
+						<div className="col-md-4">
+							<button className="btn w-100 rounded mb-2 bg-transparent border py-1 py-sm-2 text-logo text-right" type="btn">
+								<i className="fa fa-arrow-up"></i>&nbsp;ID
+							</button>
+
+							<button className="btn w-100 rounded bg-logo px-1 px-sm-2 d-flex flex-column justify-content-center align-items-between">
+								<span className="w-100 d-flex justify-content-between">
+									<span>{items.id}</span>
+									<span className="text-white">ID 7860</span>
 								</span>
-								<span className="bg-pry p-1 shadow-lg rounded bg-dark-logo ml-1">
-									0 <span>XLM</span>
-								</span>
-							</span>
-						</button>
-						<div className="d-flex flex-column">
-							<div>
-								<div className="first__line d-flex justify-content-between">
-									<span className="pl-1"></span>
-									<span className="pr-1"></span>
-								</div>
-								<div className="first__row d-flex justify-content-between">
-									<span className="d-inline-block border rounded-circle">
-										{/* <Link className="rounded-circle d-inline-block w-100 h-100" to="/"></Link> */}
+								<span className="w-100 text-white d-flex justify-content-end py-1">
+									<span className="bg-pry p-1 shadow-lg rounded bg-dark-logo">
+										$ <span>{items.idAmt}</span>
 									</span>
-									<span className="d-inline-block border rounded-circle"></span>
+									<span className="bg-pry p-1 shadow-lg rounded bg-dark-logo ml-1">
+										{items.stellarAmt} <span>XLM</span>
+									</span>
+								</span>
+							</button>
+							<div className="d-flex flex-column">
+								<div>
+									<div className="first__line d-flex justify-content-between">
+										<span className="pl-1"></span>
+										<span className="pr-1"></span>
+									</div>
+									<div className="first__row d-flex justify-content-between">
+										<span className="d-inline-block border rounded-circle">
+											{items.people[0].id === 1 && items.people[0].link ? (
+												<Link className="rounded-circle d-inline-block w-100 h-100 bg-logo" to="/home/page2"></Link>
+											) : (
+												""
+											)}
+										</span>
+										<span className="d-inline-block border rounded-circle">
+											{items.people[1].id === 2 && items.people[1].link ? (
+												<Link className="rounded-circle d-inline-block w-100 h-100 bg-logo" to="/home/page2"></Link>
+											) : (
+												""
+											)}
+										</span>
+									</div>
+								</div>
+								<div className="sec__d">
+									<div className="secd__line d-flex justify-content-between">
+										<div className="first">
+											<span className="d-inline-block pl-1"></span>
+											<span className="d-inline-block pl-1"></span>
+										</div>
+										<div className="second">
+											<span className="d-inline-block pr-1"></span>
+											<span className="d-inline-block pr-1"></span>
+										</div>
+									</div>
+									<div className="secd__row d-flex justify-content-between">
+										<div className="first">
+											<span className="d-inline-block border rounded-circle">
+												{items.people[0].followers[0].id === 1 && items.people[0].followers[0].link ? (
+													<Link
+														className="rounded-circle d-inline-block w-100 h-100 bg-pry"
+														to="/home/page2"
+													></Link>
+												) : (
+													""
+												)}
+											</span>
+											<span className="d-inline-block border rounded-circle">
+												{items.people[0].followers[1].id === 1 && items.people[0].followers[1].link ? (
+													<Link
+														className="rounded-circle d-inline-block w-100 h-100 bg-pry"
+														to="/home/page2"
+													></Link>
+												) : (
+													""
+												)}
+											</span>
+										</div>
+										<div className="second">
+											<span className="d-inline-block border rounded-circle">
+												{items.people[1].followers[0].id === 1 && items.people[1].followers[0].link ? (
+													<Link
+														className="rounded-circle d-inline-block w-100 h-100 bg-pry"
+														to="/home/page2"
+													></Link>
+												) : (
+													""
+												)}
+											</span>
+											<span className="d-inline-block border rounded-circle">
+												{items.people[1].followers[1].id === 1 && items.people[1].followers[1].link ? (
+													<Link
+														className="rounded-circle d-inline-block w-100 h-100 bg-pry"
+														to="/home/page2"
+													></Link>
+												) : (
+													""
+												)}
+											</span>
+										</div>
+									</div>
 								</div>
 							</div>
-							<div className="sec__d">
-								<div className="secd__line d-flex justify-content-between">
-									<div className="first">
-										<span className="d-inline-block pl-1"></span>
-										<span className="d-inline-block pl-1"></span>
-									</div>
-									<div className="second">
-										<span className="d-inline-block pr-1"></span>
-										<span className="d-inline-block pr-1"></span>
-									</div>
-								</div>
-								<div className="secd__row d-flex justify-content-between">
-									<div className="first">
-										<span className="d-inline-block border rounded-circle"></span>
-										<span className="d-inline-block border rounded-circle"></span>
-									</div>
-									<div className="second">
-										<span className="d-inline-block border rounded-circle"></span>
-										<span className="d-inline-block border rounded-circle"></span>
-									</div>
-								</div>
+						</div>
+
+						<div className="col-md-4 d-flex justify-content-center align-items-center flex-wrap">
+							<div className="text-center">
+								<i className="fa fa-users text-logo"></i>
+								<span className="pl-1">{items.item1}</span>
+							</div>
+							<div className="pl-1 text-center">
+								<i className="fa fa-gift text-pry"></i>
+								<span className="pl-1">{items.item2}</span>
+							</div>
+							<div className="pl-1 text-center">
+								<i className="fa fa-minus bg-pry rounded-circle"></i>
+								<span className="pl-1">{items.item3}</span>
 							</div>
 						</div>
 					</div>
 
-					<div className="col-md-4 d-flex justify-content-center align-items-center flex-wrap">
-						<div className="text-center">
-							<i className="fa fa-users text-logo"></i>
-							<span className="pl-1">951</span>
-						</div>
-						<div className="pl-1 text-center">
-							<i className="fa fa-gift text-pry"></i>
-							<span className="pl-1">2.39</span>
-						</div>
-						<div className="pl-1 text-center">
-							<i className="fa fa-minus bg-pry rounded-circle"></i>
-							<span className="pl-1">2.39</span>
-						</div>
+					<div className="d-flex flex-nowrap ab">
+						<span className="bg-logo rounded spn d-inline-block bg">15</span>
+						<span className="bg-pry text-logo spn d-inline-block rounded-circle" onClick={handleRightClick}>
+							<i className="fa fa-arrow-right"></i>
+						</span>
 					</div>
-				</div>
-
-				<div className="d-flex flex-nowrap ab">
-					<span className="bg-logo rounded spn d-inline-block bg">15</span>
-					<Link to="/home/page2" className="bg-pry text-logo spn d-inline-block rounded-circle">
-						<i className="fa fa-arrow-right"></i>
-					</Link>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	} else {
+		return <div>Loading</div>;
+	}
 };
 
 export default Info;
